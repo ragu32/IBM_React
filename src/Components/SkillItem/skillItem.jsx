4bbd4780318e5b8422c9d565ad2c8c81474eeda9
Manikandarajan-Jegadeshwaran <mjhacky@gmail.com>
2019-04-06 08:52:23 +0530

    skill item code changes

--- src/Components/SkillItem/skillItem.jsx
+++ src/Components/SkillItem/skillItem.jsx
@@ -1,11 +1,57 @@
-import React from "react"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
-const SkillItem = props => {
-    return(
-        <div>
-            <span>check</span>
-        </div>
-    );
+const styles = {
+  root: {
+    color: green[600],
+    '&$checked': {
+      color: green[500],
+    },
+  },
+  checked: {},
 };
 
-export default SkillItem;
+class CheckboxLabels extends React.Component {
+  state = {
+    checkedB: false
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <FormGroup row>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={this.state.checkedB}
+              onChange={this.handleChange('checkedB')}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label="Primary"
+        />
+      </FormGroup>
+    );
+  }
+}
+
+CheckboxLabels.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CheckboxLabels);
